1bbfe4eb8363ae0fafd4edd115d5b962c809ce5e
OZGE CAGLAYAN <oezgecaglayan@gmail.com>
2023-01-10 22:53:37 -0500

    added changes

--- src/_08_SendKeys.java
+++ src/_08_SendKeys.java
@@ -1,9 +1,10 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import utils.Driver;
 import utils.Waiter;
 
-public class SendKeys {
+public class _08_SendKeys {
     public static void main(String[] args) {
        /*
 TEST CASE
@@ -11,23 +12,28 @@ Go to https://www.wikipedia.org/
 Search for "Tesla"
 Validate the main title of the page is "Tesla"
 NOTE: Use xpath or cssSelector to locate the web elements
- */
+
 
 
         WebDriver driver = Driver.getDriver();
 
         driver.get("https://www.wikipedia.org/");
 
-        driver.findElement(By.id("searchInput")).sendKeys("Tesla");
+        driver.findElement(By.id("searchInput")).sendKeys("Tesla" + Keys.ENTER);
 
         Waiter.pause(2);
 
-        driver.findElement(By.cssSelector("button[class*='pure-button']")).click();
+   //     driver.findElement(By.cssSelector("button[class*='pure-button']")).click();
 
         System.out.println(driver.findElement(By.cssSelector(".mw-page-title-main")).getText());
 
 
         driver.quit();
+
+
+        */
+
+
 
     }
 }
